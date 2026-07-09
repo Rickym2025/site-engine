0ac54ada3f9552ea1a02b3025fe7c4207ada1cb6
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-09 10:09:26 +0200

    Update Template_il_creativo.tsx

--- src/components/Template_il_creativo.tsx
+++ src/components/Template_il_creativo.tsx
@@ -33,7 +33,7 @@ interface TemplateProps {
   hasBlog?: boolean;
 }
 
-export default function Template_il_creativo({ data, nomeCliente, slug }: TemplateProps) {
+export default function Template_il_creativo({ data, nomeCliente, slug, hasBlog }: TemplateProps) {
   const { hero, social_proof, email, indirizzo, social_fb, social_ig, piva } = data;
   const servizi = data.servizi || [];
 
