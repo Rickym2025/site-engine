d03f8b25d10467e86dd3550b5daf95b6023ff0d0
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-09 09:48:57 +0200

    Update Template_il_regista.tsx

--- src/components/Template_il_regista.tsx
+++ src/components/Template_il_regista.tsx
@@ -31,6 +31,7 @@ interface TemplateProps {
   };
   nomeCliente: string;
   slug: string;
+  hasBlog?: boolean;
 }
 
 export default function Template_il_regista({ data, nomeCliente, slug }: TemplateProps) {
