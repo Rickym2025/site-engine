e2b80ab1e4c88bbd8aee20a4230484524ffa01be
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-16 14:52:56 +0200

    Update Template_il_creativo.tsx

--- src/components/Template_il_creativo.tsx
+++ src/components/Template_il_creativo.tsx
@@ -9,6 +9,7 @@ import GoogleMap from './GoogleMap';
 import Link from 'next/link';
 import Gallery from './Gallery';
 import SocialLinks from './SocialLinks'; // ⚡ Importato il componente globale sistematico
+import ReviewsMarquee from './ReviewsMarquee';
 
 interface TemplateProps {
   data: {
@@ -265,6 +266,9 @@ export default function Template_il_creativo({ data, nomeCliente, slug, hasBlog 
           ))}
         </div>
       </section>
+
+      {/* SEZIONE RECENSIONI SCORREVOLI INTEGRATA */}
+      <ReviewsMarquee recensioni={data.recensioni} brandColor={brand_color} />
 
       {/* ========================================================================= */}
       {/* 📍 SEZIONE CONTATTO EDITORIALE CON MAPPA SPLIT-SCREEN COERENTE */}
