d9eb2a80e13e689932d34e487e2c93a7ab39fd5d
Riccardo Modena <modena.riccardo@gmail.com>
2026-07-16 14:55:45 +0200

    Update Template_l_empatico.tsx

--- src/components/Template_l_empatico.tsx
+++ src/components/Template_l_empatico.tsx
@@ -9,6 +9,7 @@ import GoogleMap from './GoogleMap';
 import Link from 'next/link';
 import Gallery from './Gallery';
 import SocialLinks from './SocialLinks'; // ⚡ Importato il componente globale sistematico
+import ReviewsMarquee from './ReviewsMarquee';
 
 interface TemplateProps {
   data: {
@@ -179,6 +180,9 @@ export default function Template_l_empatico({ data, nomeCliente, slug, hasBlog }
           </p>
         </div>
       </section>
+
+      {/* SEZIONE RECENSIONI SCORREVOLI INTEGRATA */}
+      <ReviewsMarquee recensioni={data.recensioni} brandColor={brand_color} />
 
       {/* AREE DI INTERVENTO (SERVIZI) */}
       <section id="servizi" className="py-24 px-6 max-w-6xl mx-auto relative z-10">
